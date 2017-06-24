f0bc2f9e772099117de12a0d11622640000a8afd
Thomas Sauer <t-sauer@outlook.de>
2017-06-24 23:51:15 +0200

    Added properties to speedtest model

--- src/models/speedtest.ts
+++ src/models/speedtest.ts
@@ -2,9 +2,16 @@ import { DataTypes, Sequelize } from 'sequelize';
 
 export default (sequelize: Sequelize, dataTypes: DataTypes) => {
   const Speedtest = sequelize.define('Speedtest', {
-    server_id: {
+    downloadSpeed: dataTypes.NUMBER,
+    endTime: dataTypes.NUMBER,
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
       type: dataTypes.INTEGER
-    }
+    },
+    ping: dataTypes.NUMBER,
+    startTime: dataTypes.NUMBER,
+    uploadSpeed: dataTypes.NUMBER
   });
 
   return Speedtest;
